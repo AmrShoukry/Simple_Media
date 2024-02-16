0c39d44af8b6d54a7374289baa1e0323c9f0572b
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-02-16 08:56:25 +0200

    added loginhandler

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,14 +1,15 @@
 const catchAsync = require("../utils/catchAsync");
 const User = require("../models/User");
+const { compare } = require("bcrypt");
 
 exports.handleLogin = catchAsync(async (req, res, next) => {
-  const user = await User.findOne({});
+  const user = await User.findOne({ email: req.body.email });
 
-  if (user) {
+  if (user && compare(req.body.email, user.password)) {
     return res.status(200).json({
       status: "success",
     });
   }
 
-  return next("User not found");
+  return next("Incorrect credentials");
 });
