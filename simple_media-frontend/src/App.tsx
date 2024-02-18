7616d3c1806c03c0215c9be9a4c5825f68d034d3
samfrexz619 <samfrexz69@gmail.com>
2024-02-18 13:53:53 +0100

    add login page

--- simple_media-frontend/src/App.tsx
+++ simple_media-frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
+// import './App.css'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Login from './pages/Login'
