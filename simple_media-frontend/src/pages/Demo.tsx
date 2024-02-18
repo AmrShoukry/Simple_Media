d8839a817c7fb99f23d7901d0f88281bf241c0cc
samfrexz619 <samfrexz69@gmail.com>
2024-02-18 11:05:15 +0100

    add dependencies: sass, sass-loader

---
+++ simple_media-frontend/src/pages/Demo.tsx
@@ -0,0 +1,12 @@
+import TextInput from "../components/ui/input/TextInput"
+
+
+export const Demo = () => {
+  return (
+    <div>
+      <div className="py-5">
+        <TextInput placeholder="enter text" />
+      </div>
+    </div>
+  )
+}
