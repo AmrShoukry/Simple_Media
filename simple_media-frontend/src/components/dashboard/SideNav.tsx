ed19b56422dc0c7783bbe68de0fab2712e32a353
samfrexz619 <samfrexz69@gmail.com>
2024-02-25 08:33:59 +0100

    add links to sideNav comp

--- simple_media-frontend/src/components/dashboard/SideNav.tsx
+++ simple_media-frontend/src/components/dashboard/SideNav.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { GoHome } from "react-icons/go";
+import { CiLogout } from "react-icons/ci";
+import { FaRegUser } from "react-icons/fa";
+
+
+
 
 interface Props {
 
@@ -7,8 +14,29 @@ interface Props {
 const SideNav: React.FC<Props> = () => {
   return (
     <aside className='w-280 bg-pry fixed inset-y-0 flex flex-col text-textPry'>
-      <nav className='overflow-y-auto w-full'>
-        <div></div>
+      <nav className='overflow-y-auto w-full pt-7'>
+        <Link to={'/home'} className='text-[28px] pl-9 font-extrabold italic'>Simple_Media</Link>
+
+        <ul className='py-10 pl-9'>
+          <li className='text-16 font-bold'>
+            <Link to={'/home'} className='flex items-center py-4 gap-x-2'>
+              <GoHome  />
+              <span>Home</span>
+            </Link>
+          </li>
+          <li className='text-16 font-bold'>
+            <Link to={'/home'} className='flex items-center py-4 gap-x-2'>
+              <FaRegUser  />
+              <span>My Profile</span>
+            </Link>
+          </li>
+          <li className='text-16 font-bold'>
+            <button className='flex items-center py-4 gap-x-2'>
+              <CiLogout  />
+              <span>Logout</span>
+            </button>
+          </li>
+        </ul>
       </nav>
     </aside>
   );
